5ced3094b0685e5a0a796f81fcbd9b9aedc465f1
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-05 11:53:55 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def dictionary 
+  substitutes = {
+    "hello" => "hi",
+    "to" => '2',
+    "two" => '2', 
+    "too" => '2', 
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
+    "and" => "&"
+  }
+  
+end
+
+def word_substituter(tweet)
+  tweet_words = tweet.split(" ")
+  words_to_change = dictionary.keys
+  
+  
+  tweet_words.collect do |word|
+    for i in words_to_change
+      if word == i
+        word = dictionary[i]  
+
+      end
+      
+    end
+
+  end
+  tweet_words.join(" ")
+
+end
